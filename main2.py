975233c3653654886e4e7dbc0ab73de391555de7
indapa <indapa@gmail.com>
2024-03-15 14:22:33 -0700

    updated session state to toggle between goals

--- main2.py
+++ main2.py
@@ -4,8 +4,43 @@ from lida.datamodel import Goal
 import os
 import pandas as pd
 
+def init_session_state():
+    if 'second_submit' not in st.session_state:
+        st.session_state.second_submit = False
+    if 'selected_dataset_label' not in st.session_state:
+        st.session_state.selected_dataset_label = None
+    if 'selected_dataset' not in st.session_state:
+        st.session_state.selected_dataset = None
+    if 'selected_model' not in st.session_state:
+        st.session_state.selected_model = None
+    if 'temperature' not in st.session_state:
+        st.session_state.temperature = None
+    if 'use_cache' not in st.session_state:
+        st.session_state.use_cache = None
+    if 'num_goals' not in st.session_state:
+        st.session_state.num_goals = None
+    if 'user_goal' not in st.session_state:
+        st.session_state.user_goal = None
+    
+    
+    
+    
+def form_callback():
+    #st.write("form callback")
+    st.write("selected dataset label: ", st.session_state.selected_dataset)
+    st.write("selected dataset: ", st.session_state.selected_dataset)
+    st.write("selected model: ", st.session_state.selected_model)
+    st.write("Num goals:" , st.session_state.num_goals)
+    st.write("Temperature: ", st.session_state.temperature)
+    
+    st.write("second_submit: ", st.session_state.second_submit)
+    #st.write("Selected goal: ", st.session_state.selected_item)
+    #st.write("User goal: ", st.session_state.user_goal)
 
 
+init_session_state()
+
+selected_library = "seaborn"
 # make data dir if it doesn't exist
 os.makedirs("data", exist_ok=True)
 
@@ -13,6 +48,11 @@ st.set_page_config(
     page_title="Exploratory Analysis with LIDA",
     page_icon="📊",
 )
+
+openai_key= st.secrets["openai_key"]
+selected_dataset = None
+user_goal = None
+submitted = None
 models = ["gpt-3.5-turbo-16k"]
 datasets = [
         
@@ -20,6 +60,8 @@ datasets = [
         {"label": "Titanic", "url": "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv"},
         {"label": "Penguins", "url": "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/penguins.csv"},
         ]
+
+
 
 summarization_methods = [
         {"label": "llm",
@@ -36,116 +78,111 @@ st.write("## Exploratory Data Analysis with LIDA 📊  :bulb:")
 lida=None
 
 
-openai_key= st.secrets["openai_key"]
-with st.sidebar:
-    
-    # make a form to submit the key
-    
-    #openai_key = st.text_input("Enter OpenAI API key:")    
-    
-    selected_model = st.selectbox(
-            'Choose a model',
-            options=models,
-            index=0
-            )
-    temperature = st.slider(
-            "Temperature",
-            min_value=0.0,
-            max_value=1.0,
-            value=0.0)
-    
+
+
+
+with st.sidebar:   
+    with st.form(key='input_form'):
         
-    use_cache = st.checkbox("Use cache", value=True)
-    selected_dataset_label = st.selectbox(
+        selected_dataset_label = st.selectbox(
             'Choose a dataset',
             options=[dataset["label"] for dataset in datasets],
-            index=1
+            index=1,
+            key="selected_dataset_label"
         )
-    num_goals = st.slider(
+        
+       
+        selected_dataset = datasets[[dataset["label"]
+            for dataset in datasets].index(selected_dataset_label)]["url"]
+
+        if not selected_dataset:
+            st.info("To continue, select a dataset from the sidebar on the left or upload your own.")
+    
+        st.session_state.selected_dataset = selected_dataset
+    
+        selected_model = st.selectbox(
+                'Choose a model',
+                options=models,
+                index=0,
+                key="selected_model"
+            )
+            
+        temperature = st.slider(
+                "Temperature",
+                min_value=0.0,
+                max_value=1.0,
+                value=0.0, 
+                key="temperature")
+    
+        
+        use_cache = st.checkbox("Use cache", value=True, key="use_cache")
+   
+   
+        num_goals = st.slider(
                 "Number of goals to generate",
                 min_value=1,
                 max_value=10,
-                value=4)
+                value=4, 
+                key="num_goals")
     
-    
-    user_goal = st.sidebar.text_input("Describe Your Goal")
         
-
-
-    selected_method_label = st.selectbox(
-            'Choose a method',
+        user_goal = st.text_input("Describe Your Goal", key="user_goal")
+        
+       
+        selected_method_label = st.selectbox(
+            'Choose a summarization method',
             options=[method["label"] for method in summarization_methods],
-            index=0
-        )
+            index=0,
+            key="selected_method_label"
+            )
 
 
-    selected_method = summarization_methods[[
-            method["label"] for method in summarization_methods].index(selected_method_label)]["label"]
+        selected_method = summarization_methods[[
+            method["label"] for method in summarization_methods].index(st.session_state.selected_method_label)]["label"]
+        st.session_state.selected_method = selected_method
 
         # add description of selected method in very small font to sidebar
-    selected_summary_method_description = summarization_methods[[
+        selected_summary_method_description = summarization_methods[[
             method["label"] for method in summarization_methods].index(selected_method_label)]["description"]
 
-    if selected_method:
-        st.markdown(
+        if selected_method:
+            st.markdown(
             f"<span> {selected_summary_method_description} </span>",
-            unsafe_allow_html=True)
+                unsafe_allow_html=True)
 
-    num_visualizations = st.sidebar.slider(
-                "Number of visualizations to generate",
-                min_value=1,
-                max_value=3,
-                value=2)
-        
+        num_visualizations=1
+               
+        submitted= st.form_submit_button("Submit", on_click=form_callback)
     
-    #visualization_libraries = ["seaborn", "matplotlib"]
-
-    selected_library = "seaborn"
-    #selected_library = st.sidebar.selectbox(
-    #            'Choose a visualization library',
-    #            options=visualization_libraries,
-    #            index=0
-    #        )
-
-
     
-display_key = openai_key[:2] + "*" * (len(openai_key) - 5) + openai_key[-3:]
-selected_dataset = datasets[[dataset["label"]
-                                     for dataset in datasets].index(selected_dataset_label)]["url"]
-st.write("GPT mode", selected_model)
 
-st.write("Datset:", selected_dataset_label)
-st.write("Temperature:", temperature)
-st.write("Number of goals:", num_goals)
-if user_goal:
-    st.write("User Goal:", user_goal)
-st.write("Use cache:", use_cache)
-st.write("Summarization method:", selected_method_label)
-st.write("Number of visualizations:", num_visualizations)
-st.write("Visualization library:", selected_library)
+    if submitted:
+        st.session_state.second_submit = True
+    
 
+if st.session_state.second_submit == True:
 
-lida = Manager(text_gen=llm("openai", api_key=openai_key))
-textgen_config = TextGenerationConfig(
+    lida = Manager(text_gen=llm("openai", api_key=openai_key))
+    textgen_config = TextGenerationConfig(
         n=1,
-        temperature=temperature,
-        model=selected_model,
-        use_cache=use_cache)
+        temperature=st.session_state.temperature,
+        model=st.session_state.selected_model,
+        use_cache=st.session_state.use_cache)
     
-st.write("## Summary")
-if openai_key is None:
-     st.error("Please enter an OpenAI API key")
+    st.write("## Summary")
+    
+
 
     # **** lida.summarize *****
-summary = lida.summarize(
-        selected_dataset,
-        summary_method=selected_method,
+    summary = lida.summarize(
+        st.session_state.selected_dataset,
+        summary_method=st.session_state.selected_method,
         textgen_config=textgen_config)
 
-if "dataset_description" in summary:
+    if "dataset_description" in summary:
         st.write(summary["dataset_description"])
 
-if "fields" in summary:
+    if "fields" in summary:
         fields = summary["fields"]
         nfields = []
         for field in fields:
@@ -161,66 +198,72 @@ if "fields" in summary:
             nfields.append(flatted_fields)
         nfields_df = pd.DataFrame(nfields)
         st.write(nfields_df)
-else:
+    else:
         st.write(str(summary))
 
-#generate goals
+    #generate goals
 
-goals = lida.goals(summary, n=num_goals, textgen_config=textgen_config)
-default_goal = goals[0].question
-goal_questions = [goal.question for goal in goals]
-
-#append user goal
-if user_goal:
-    new_goal = Goal(index=0,question=user_goal, visualization=user_goal, rationale="")
-    goals.append(new_goal)
-    goal_questions.append(new_goal.question)
+    goals = lida.goals(summary, n=num_goals, textgen_config=textgen_config)
+    default_goal = goals[0].question
+    goal_questions = [goal.question for goal in goals]
 
 
+    #append user goal
+    if user_goal:
+        new_goal = Goal(index=0,question=user_goal, visualization=user_goal, rationale="")
+        goals.append(new_goal)
+        goal_questions.append(new_goal.question)
+    #st.write(goal_questions)
 
-st.write("## Goals")
-#if st.session_state['form_one_complete']:
+
+
+    st.write("## Goals")
     
-selected_goal = st.selectbox('Choose a generated goal', options=goal_questions, index=0)
-selected_goal_index = goal_questions.index(selected_goal)
+    selected_goal = st.selectbox('Choose a generated goal', options=goal_questions, index=0)
+    selected_goal_index = goal_questions.index(selected_goal)
     
-st.write(goals[selected_goal_index])
+    #st.write(goals[selected_goal_index])
         
-selected_goal_object = goals[selected_goal_index]
+    selected_goal_object = goals[selected_goal_index]
+    st.session_state.selected_item = selected_goal_object
+    
+    
+    st.write(st.session_state.selected_item)
+    
+    # visualize goal
+    if  st.session_state.selected_item:
+        st.write("## Visualizations")
+        
 
-# visualize goal
-if selected_goal_object:
-    st.write("## Visualizations")
-    st.write(goal_questions.index(selected_goal))    
-
-    textgen_config = TextGenerationConfig(
+        textgen_config = TextGenerationConfig(
                 n=num_visualizations, temperature=temperature,
                 model=selected_model,
                 use_cache=use_cache)
 
             # **** lida.visualize *****
-    visualizations = lida.visualize(
+        visualizations = lida.visualize(
                 summary=summary,
-                goal=selected_goal_object,
+                #goal=selected_goal_object,
+                goal=st.session_state.selected_item,
                 textgen_config=textgen_config,
                 library=selected_library) 
     
-    st.write("total visualizations", len(visualizations))   
-    viz_titles = [f'Visualization {i+1}' for i in range(len(visualizations))]
+        #st.write("total visualizations", len(visualizations))   
+        viz_titles = [f'Visualization {i+1}' for i in range(len(visualizations))]
 
-    selected_viz_title = st.selectbox('Choose a visualization', options=viz_titles, index=0)
-    
-    selected_viz = visualizations[viz_titles.index(selected_viz_title)]
+        #selected_viz_title = st.selectbox('Choose a visualization', options=viz_titles, index=0)
+        selected_viz_title = 'Visualization 1'
+        selected_viz = visualizations[viz_titles.index(selected_viz_title)]
 
-    if selected_viz.raster:
-        from PIL import Image
-        import io
-        import base64
+        if selected_viz.raster:
+            from PIL import Image
+            import io
+            import base64
 
-        imgdata = base64.b64decode(selected_viz.raster)
-        img = Image.open(io.BytesIO(imgdata))
-        st.image(img, caption=selected_viz_title, use_column_width=True)
+            imgdata = base64.b64decode(selected_viz.raster)
+            img = Image.open(io.BytesIO(imgdata))
+            st.image(img, caption=selected_viz_title, use_column_width=True)
 
-        st.write("### Visualization Code")
-        st.code(selected_viz.code)
+            st.write("### Visualization Code")
+            st.code(selected_viz.code)
      
